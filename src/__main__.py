cc8ea53a25396e2d24d5382ffa8b8f81dabccfef
ThijmHacks <148696012+ThijmHacks@users.noreply.github.com>
2025-04-08 20:04:00 +0200

    Made voice to text converter

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,21 @@
-import os
-os.system("cls")
+import speech_recognition as sr
 
-print("Hello World")
+def user():
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Please say something: ")
+        audio = recognizer.listen(source)
+
+    try:
+        print("Recognizing...")
+        command = recognizer.recognize_google(audio)
+        print(f"You said to me: {command}")
+        return command.lower()
+    except sr.UnknownValueError:
+        print("Can you repeat that?")
+        return None
+    except sr.RequestError:
+        print("I cannot connect to your service.")
+        return None
+
+user()
